b5045f5e7b8ee2a158afc91ef58be94ef76f3278
zoecq001 <zoecq001@gmail.com>
2019-12-24 21:58:06 +0800

    monitor

---
+++ app/src/main/java/com/team/focus/data/model/monitor/monitor.java
@@ -0,0 +1,9 @@
+package com.team.focus.data.model.monitor;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.Context;
+
+
+public class monitor {
+
+}
---
+++ app/src/main/java/com/team/focus/ui/monitor/MonitorActivity.java
@@ -0,0 +1,5 @@
+package com.team.focus.ui.monitor;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MonitorActivity extends AppCompatActivity {
+}
