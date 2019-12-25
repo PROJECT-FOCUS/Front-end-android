5c314888d99d56ff81fe700de6c6889651ac42c9
zsxing99 <zsxing99@gmail.com>
2019-12-25 06:55:31 -0800

    UI -upgrade overview fragment xml

--- app/src/main/java/com/team/focus/ui/overview/OverviewFragment.java
+++ app/src/main/java/com/team/focus/ui/overview/OverviewFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,23 +15,37 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.team.focus.R;
+import com.team.focus.data.model.SharedPreferenceAccessUtils;
 
 public class OverviewFragment extends Fragment {
 
-    private OverviewViewModel overviewViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        overviewViewModel =
-                ViewModelProviders.of(this).get(OverviewViewModel.class);
         View root = inflater.inflate(R.layout.fragment_overview, container, false);
-        final TextView textView = root.findViewById(R.id.text_overview);
-        overviewViewModel.getText().observe(this, new Observer<String>() {
+
+        final TextView textViewUser = root.findViewById(R.id.overview_header_username);
+        final TextView textViewActive = root.findViewById(R.id.active_hours);
+        final TextView textViewInterval = root.findViewById(R.id.interval);
+        final ImageButton addMonitor = root.findViewById(R.id.bntAddMonitor);
+
+        int startTime = SharedPreferenceAccessUtils.getTimeIntervalStart(root.getContext());
+        int endTime = SharedPreferenceAccessUtils.getTimeIntervalEnd(root.getContext());
+        int interval = endTime - startTime;
+        String textActive = "Active from " + startTime + " to " + endTime;
+        String textInterval = interval + " hours in total";
+
+        textViewActive.setText(textActive);
+        textViewInterval.setText(textInterval);
+
+        textViewUser.setText(SharedPreferenceAccessUtils.getUsername(root.getContext()));
+
+        addMonitor.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                // ToDO : start add monitor object(s)
             }
         });
+
         return root;
     }
 }
