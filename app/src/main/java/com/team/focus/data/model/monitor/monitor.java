71bcd84f491236875767acbcea28e916e990fa29
Ruxuan Ma <ruxuan.m@gmail.com>
2019-12-27 13:02:43 -0800

    Revert "Merge branches 'UI', 'monitor' and 'setup' of https://github.com/PROJECT-FOCUS/Front-end-android into setup"
    This reverts commit 9f9b37d15d093dae70f83f2d6f21d2523cecdf0f, reversing
    changes made to ea96128f0756de8f58a9e624aaa197314e24eb7a.
    
    Revert accidental merge.

--- app/src/main/java/com/team/focus/data/model/monitor/monitor.java
+++
@@ -1,9 +0,0 @@
-package com.team.focus.data.model.monitor;
-import android.app.usage.UsageStats;
-import android.app.usage.UsageStatsManager;
-import android.content.Context;
-
-
-public class monitor {
-
-}
