653652610d022eccb50e3b5d7798a81095804b62
zsxing99 <zsxing99@gmail.com>
2019-12-28 04:24:27 -0800

    Splash screen created

--- app/src/main/java/com/team/focus/LauncherActivity.java
+++ app/src/main/java/com/team/focus/LauncherActivity.java
@@ -1,14 +1,20 @@
 package com.team.focus;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.AppOpsManager;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -16,10 +22,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class LauncherActivity extends AppCompatActivity {
 
+    private Activity activity;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        activity = this;
         if (!checkPermission()) {
             Intent intent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS);
             Toast.makeText(this, "Please turn usage access permission for FOCUS",
@@ -48,17 +56,50 @@ public class LauncherActivity extends AppCompatActivity {
         SharedPreferences sharedPreferences = getSharedPreferences("FOCUS", Context.MODE_PRIVATE);
         boolean didUserLogin = sharedPreferences.getBoolean("didUserLogin", false);
 
-        Intent nextActivity = didUserLogin ? new Intent(this, MainActivity.class) :
+        final Intent nextActivity = didUserLogin ? new Intent(this, MainActivity.class) :
                 new Intent(this, LoginActivity.class);
         if (didUserLogin) {
             String username = sharedPreferences.getString("username", "");
             String welcome = getString(R.string.welcome_back) + username;
             Toast.makeText(getApplicationContext(), welcome, Toast.LENGTH_LONG).show();
+            startActivity(nextActivity);
+            finish();
         }
 
-        startActivity(nextActivity);
-        setResult(Activity.RESULT_OK);
-        finish();
+        setContentView(R.layout.splash_screen);
+        final Button start = findViewById(R.id.start_btn);
+        final TextView term = findViewById(R.id.terms);
+        final CheckBox checkBox = findViewById(R.id.checkBox3);
+
+        term.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setTitle("Term of use");
+                builder.setMessage(R.string.terms);
+                builder.setPositiveButton("Accept",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                checkBox.setChecked(true);
+                                dialog.dismiss();
+                            }
+                        });
+                builder.show();
+            }
+        });
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (checkBox.isChecked()) {
+                    activity.startActivity(nextActivity);
+                    activity.finish();
+                } else {
+                    Toast.makeText(activity, "Please review Terms of use", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private boolean checkPermission() {
